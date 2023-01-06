71a0bd142494ba591e9388fd1774212e2fc61c03
keekerr <keeley.s.sprouse@gmail.com>
2023-01-05 23:54:05 -0500

    Code updated

--- public/assets/apiRoutes/index.js
+++ public/assets/apiRoutes/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const {createNewNote, updateDb} = require("../../lib/notes");
 const { v4: uuidv4 } = require('uuid');
 const {notes} = require("../../db/db.json");
 
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const apiRoutes = require('public\assets\apiRoutes');
 const htmlRoutes = require('public\assets\htmlRoutes');
 
 app.use(express.static('public'));
 
+// Source:https://www.geeksforgeeks.org/express-js-express-urlencoded-function/
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 
+// Added code to specify root directory 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
